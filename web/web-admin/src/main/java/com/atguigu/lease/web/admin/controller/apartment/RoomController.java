7b7bf5cc342968d43c6a3f5973aa95133bbb8736
Administrator <18216757927@163.com>
2026-07-06 16:48:12 +0800

    第十五次提交：公寓管理-根据公寓id修改房间发布状态及查询房间列表

--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/RoomController.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/RoomController.java
@@ -4,13 +4,17 @@ package com.atguigu.lease.web.admin.controller.apartment;
 import com.atguigu.lease.common.result.Result;
 import com.atguigu.lease.model.entity.RoomInfo;
 import com.atguigu.lease.model.enums.ReleaseStatus;
+import com.atguigu.lease.web.admin.service.RoomInfoService;
 import com.atguigu.lease.web.admin.vo.room.RoomDetailVo;
 import com.atguigu.lease.web.admin.vo.room.RoomItemVo;
 import com.atguigu.lease.web.admin.vo.room.RoomQueryVo;
 import com.atguigu.lease.web.admin.vo.room.RoomSubmitVo;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,6 +23,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/admin/room")
 public class RoomController {
+
+    @Autowired
+    private RoomInfoService roomInfoService;
 
     @Operation(summary = "保存或更新房间信息")
     @PostMapping("saveOrUpdate")
@@ -47,13 +54,23 @@ public class RoomController {
     @Operation(summary = "根据id修改房间发布状态")
     @PostMapping("updateReleaseStatusById")
     public Result updateReleaseStatusById(Long id, ReleaseStatus status) {
+        LambdaUpdateWrapper<RoomInfo> roomInfoLambdaUpdateWrapper = new LambdaUpdateWrapper<>();
+        roomInfoLambdaUpdateWrapper.eq(RoomInfo::getId, id);
+        roomInfoLambdaUpdateWrapper.eq(RoomInfo::getIsRelease, status);
+        roomInfoService.update(roomInfoLambdaUpdateWrapper);
         return Result.ok();
     }
 
     @GetMapping("listBasicByApartmentId")
     @Operation(summary = "根据公寓id查询房间列表")
     public Result<List<RoomInfo>> listBasicByApartmentId(Long id) {
-        return Result.ok();
+        // 查询指定公寓id的房间列表
+        // 再根据房间列表筛选已发布的房间列表
+        LambdaQueryWrapper<RoomInfo> roomInfoLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        roomInfoLambdaQueryWrapper.eq(RoomInfo::getApartmentId, id);
+        roomInfoLambdaQueryWrapper.eq(RoomInfo::getIsRelease, ReleaseStatus.RELEASED);
+        List<RoomInfo> roomInfoList = roomInfoService.list(roomInfoLambdaQueryWrapper);
+        return Result.ok(roomInfoList);
     }
 
 }
