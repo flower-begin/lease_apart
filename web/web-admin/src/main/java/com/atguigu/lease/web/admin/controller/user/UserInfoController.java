9fcb47fc9bf60a9fffe924ba18e6a320a9ac391c
Administrator <18216757927@163.com>
2026-07-07 13:51:38 +0800

    第二十二次提交：后台用户管理-单表分页查询用户信息以及更改用户账号状态

--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/user/UserInfoController.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/user/UserInfoController.java
@@ -4,10 +4,15 @@ package com.atguigu.lease.web.admin.controller.user;
 import com.atguigu.lease.common.result.Result;
 import com.atguigu.lease.model.entity.UserInfo;
 import com.atguigu.lease.model.enums.BaseStatus;
+import com.atguigu.lease.web.admin.service.UserInfoService;
 import com.atguigu.lease.web.admin.vo.user.UserInfoQueryVo;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "用户信息管理")
@@ -15,15 +20,27 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/admin/user")
 public class UserInfoController {
 
+    @Autowired
+    private UserInfoService userInfoService;
     @Operation(summary = "分页查询用户信息")
     @GetMapping("page")
     public Result<IPage<UserInfo>> pageUserInfo(@RequestParam long current, @RequestParam long size, UserInfoQueryVo queryVo) {
-        return Result.ok();
+        IPage<UserInfo> page = new Page<>(current, size);
+        LambdaQueryWrapper<UserInfo> userInfoLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        userInfoLambdaQueryWrapper.eq(queryVo.getStatus() != null, UserInfo::getStatus, queryVo.getStatus());
+        userInfoLambdaQueryWrapper.eq(queryVo.getPhone() != null, UserInfo::getPhone, queryVo.getPhone());
+        // 单表条件分页查询，直接调用mybatis-plus方法即可
+        userInfoService.page(page, userInfoLambdaQueryWrapper);  // 参数一存放分页参数，参数二存放条件
+        return Result.ok(page);
     }
 
     @Operation(summary = "根据用户id更新账号状态")
     @PostMapping("updateStatusById")
     public Result updateStatusById(@RequestParam Long id, @RequestParam BaseStatus status) {
+        LambdaUpdateWrapper<UserInfo> userInfoLambdaUpdateWrapper = new LambdaUpdateWrapper<>();
+        userInfoLambdaUpdateWrapper.eq(UserInfo::getId, id);
+        userInfoLambdaUpdateWrapper.set(UserInfo::getStatus, status);
+        userInfoService.update(userInfoLambdaUpdateWrapper);
         return Result.ok();
     }
 }
